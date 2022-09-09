f9f151be83d09d4c460e931f17e52c4b91407973
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2022-09-08 22:02:10 -0300

    creacion de frases

---
+++ scripts/frases.js
@@ -0,0 +1,362 @@
+const dunder=[
+    {
+        id: 101,
+        content: "Si tuviera una pistola con dos balas y estuviera en una habitación con Hitler, Bin Laden y Toby, le dispararía a Toby dos veces.",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 102,
+        content: "Ocasionalmente, golpeo a alguien con mi auto. Entonces demándame.",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 103,
+        content: "¡Todos manténganse tranquilos! ¡Quédense p#%&mente tranquilos!",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 104,
+        content: "Me... Declaro. ¡Bancarrota!",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 105,
+        content: "Y sabía exactamente qué hacer. Pero en un sentido mucho más real, no tenía ni idea de qué hacer.",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 106,
+        content: "Siento que todos mis hijos crecieron y luego se casaron entre ellos... Es el sueño de todo padre...",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 107,
+        content: "¡Dwight, zorra ignorante!",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 108,
+        content: "Bueno, feliz cumpleaños Jesús. Lo siento, que tu fiesta sea tan aburrida...",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 109,
+        content: "A veces empiezo una oración y ni siquiera sé a dónde va. Solo espero encontrar el sentido en el camino...",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 110,
+        content: "Hay ciertas cosas que un jefe no comparte con sus empleados: Su salario, eso los deprimiría. Su cama... Y no les voy a decir que voy a leer sus correos electrónicos.",
+        character: {
+        id: "Michael",
+        firstname: "Michael",
+        lastname: "Scott",
+        },
+    },
+    {
+        id: 111,
+        content: "Una vez cada hora, alguien está involucrado en una estafa por Internet. Ese hombre es Michael Scott.",
+        character: {
+        id: "Pam",
+        firstname: "Pam",
+        lastname: "Beesly",
+        },
+    },
+    {
+        id: 112,
+        content: "Es el día de la revisión del desempeño en toda la empresa. El año pasado, mi revisión de desempeño comenzó cuando Michael me preguntó cuáles eran mis esperanzas y sueños, y terminó cuando él me dijo que podía levantar 190 libras en el banco. Entonces, realmente no sé qué esperar.",
+        character: {
+        id: "Pam",
+        firstname: "Pam",
+        lastname: "Beesly",
+        },
+    },
+    {
+        id: 113,
+        content: "Siento a Dios en este Chili's esta noche.",
+        character: {
+        id: "Pam",
+        firstname: "Pam",
+        lastname:"Beesly",
+        },
+    },
+    {
+        id: 114,
+        content: "Hay mucha belleza en las cosas ordinarias. ¿No es ese el punto?",
+        character: {
+        id: "Pam",
+        firstname: "Pam",
+        lastname: "Beesly",
+        },
+    },
+    {
+        id: 115,
+        content: "Dos ojos. Dos orejas. una barbilla Una boca. Diez dedos. Dos pezones. Un trasero, dos rótulas, un pene. Acabo de describirles el monstruo del lago Ness. ¿Y la recompensa por su captura? Todas las riquezas de Escocia. Así que tengo una pregunta: ¿Por qué estás aquí?",
+        character: {
+        id: "Creed",
+        firstname: "Creed",
+        lastname: "Bratton",
+        },
+    },
+    {
+        id: 116,
+        content: "¡Estoy inmensamente orgulloso de lo que hice por esa tortuga!",
+        character: {
+        id: "Kevin",
+        firstname: "Kevin",
+        lastname: "Malone",
+        },
+    },
+    {
+        id: 117,
+        content: "Me despierto todas las mañanas en una cama que es demasiado pequeña, llevo a mi hija a una escuela que es demasiado cara y luego voy a trabajar a un trabajo por el que me pagan muy poco. ¿Pero el día del pretzel? Bueno, me gusta el día de los pretzels.",
+        character: {
+        id: "Stanley",
+        firstname: "Stanley",
+        lastname: "Hudson",
+        },
+    },
+    {
+        id: 118,
+        content: "¡¿Acaso tartamudie?!",
+        character: {
+        id: "Stanley",
+        firstname: "Stanley",
+        lastname: "Hudson",
+        },
+    },
+    {
+        id: 119,
+        content: "El año pasado, Creed me preguntó cómo configurar un blog. Queriendo proteger al mundo de la exposición al cerebro de Creed, abrí un documento de Word en su computadora y puse una dirección en la parte superior. He leído algo de eso. Incluso para Internet, es... bastante impactante.",
+        character: {
+        id: "Ryan",
+        firstname: "Ryan",
+        lastname: "Howard",
+        },
+    },
+    {
+        id: 120,
+        content: "No hablo basura, hablo bofetada. Son totalmente diferentes. La charla basura es hipotética, como: Tu mamá es tan gorda que puede comer Internet. Pero la palabrería está sucediendo como en este momento. Como: Eres feo y lo sé a ciencia cierta porque tengo la evidencia justo ahí.",
+        character: {
+        id: "Kelly",
+        firstname: "Kelly",
+        lastname: "Kapoor",
+        },
+    },
+    {
+        id: 121,
+        content: "Tengo un montón de preguntas. Número uno, ¿cómo te atreves?",
+        character: {
+        id: "Kelly",
+        firstname: "Kelly",
+        lastname: "Kapoor",
+        },
+    },
+    {
+        id: 123,
+        content: "Hoy tuve sentimientos por un compañero de trabajo que no había tenido en años... En mi defensa, estaba agarrando mi entrepierna con bastante agresividad en ese momento.",
+        character: {
+        id: "Jim",
+        firstname: "Jim",
+        lastname: "Halpert",
+        },
+    },
+    {
+        id: 124,
+        content: "Bears, Beets, Battlestar Galactica.",
+        character: {
+        id: "Jim",
+        firstname: "Jim",
+        lastname: "Halpert",
+        },
+    },
+    {
+        id: 125,
+        content: "Es verdad. Estoy teniendo una fiesta. Tengo tres cajas de cerveza importada, una máquina de karaoke y no invité a Michael. Así que tres ingredientes para una gran fiesta. Y no es nada personal, solo creo que si él estuviera allí, la gente no podría relajarse, y ya sabes, divertirse, y mi compañero de cuarto quiere conocer a todos. Porque estoy bastante seguro de que piensa que me estoy inventando a Dwight. *suspira* Él es muy real.",
+        character: {
+        id: "Jim",
+        firstname: "Jim",
+        lastname: "Halpert",
+        },
+    },
+    {
+        id: 126,
+        content: "Desearía que hubiera una manera de saber que estás en los buenos viejos tiempos, antes de que realmente los hayas dejado.",
+        character: {
+        id: "Andy",
+        firstname: "Andy",
+        lastname: "Bernard",
+        },
+    },
+    {
+        id: 127,
+        content: "No puedes dejar que una chica se sienta bien consigo misma. Te resultará contraproducente. Cada cumplido tiene que ser ambiguo. 'Oh, me gusta tu vestido, pero me gustaría más si tuvieras un cabello más bonito.'",
+        character: {
+        id: "Andy",
+        firstname: "Andy",
+        lastname: "Bernard",
+        },
+    },
+    {
+        id: 128,
+        content: "Seré el número dos aquí en Scranton en seis semanas. ¿Cómo? Repetición de nombres, reflejo de personalidad y nunca romper un apretón de manos. Siempre estoy pensando un paso adelante. Como un carpintero que hace escaleras.",
+        character: {
+        id: "Andy",
+        firstname: "Andy",
+        lastname: "Bernard",
+        },
+    },
+    {
+        id: 129,
+        content: "Lamento haberte molestado con mi amistad.",
+        character: {
+        id: "Andy",
+        firstname: "Andy",
+        lastname: "Bernard",
+        },
+    },
+    {
+        id: 130,
+        content: "Cada vez que estoy a punto de hacer algo, pienso: '¿Haría eso un idiota?' Y si lo hicieran, yo no hago esa cosa.",
+        character: {
+        id: "Dwight",
+        firstname: "Dwight",
+        lastname: "Schrute",
+        },
+    },
+    {
+        id: 131,
+        content: "Tiene que ser oficial, y tiene que ser orina.",
+        character: {
+        id: "Dwight",
+        firstname: "Dwight",
+        lastname: "Schrute",
+        },
+    },
+    {
+        id: 132,
+        content: "He sido el número 2 de Michael durante unos 5 años. Y hacemos un gran equipo. Somos como uno de esos clásicos equipos famosos. Él es como Mozart y yo soy como el amigo de Mozart. No! Soy como Butch Cassidy y Michael es como Mozart. Si intentas herir a Mozart; Vas a recibir una bala en tu cabeza por cortesía de Butch Cassidy.",
+        character: {
+        id: "Dwight",
+        firstname: "Dwight",
+        lastname: "Schrute",
+        },
+    },
+    {
+        id: 133,
+        content: "Las insignias de identificacion están muy atrasadas. La seguridad en este parque de oficinas es una broma. El año pasado, vine a trabajar con mi pistola de puntas en una bolsa de lona. Me senté en mi escritorio todo el día, con un rifle que dispara papas a 60 libras por pulgada cuadrada. ¿Te imaginas si estuviera trastornado?",
+        character: {
+        id: "Dwight",
+        firstname: "Dwight",
+        lastname: "Schrute",
+        },
+    },
+    {
+        id: 134,
+        content: "Si rezas lo suficiente, puedes convertirte en una persona que le gustan los gatos.",
+        character: {
+        _id: "Angela",
+        firstname: "Angela",
+        lastname: "Martin",
+        },
+    },
+    {
+        id: 135,
+        content: "Sé que la paciencia y la lealtad son rasgos buenos y virtuosos. Pero a veces creo que solo necesitas crecer como un par.",
+        character: {
+        id: "Angela",
+        firstname: "Angela",
+        lastname: "Martin",
+        },
+    },
+    {
+        id: 136,
+        content: "No me di cuenta de con cuántas de las opiniones de Ángela estaba de acuerdo... hasta que trató de romperme las rótulas por acostarme con su esposo.",
+        character: {
+        id: "Oscar",
+        firstname: "Oscar",
+        lastname: "Martinez",
+        },
+    },
+    {
+        id: 137,
+        content: "Me alegro de que Michael esté recibiendo ayuda. Tiene muchos problemas y es estúpido.",
+        character: {
+        id: "Phyllis",
+        firstname: "Phyllis",
+        lastname: "Lapin",
+        },
+    },
+    {
+        id: 138,
+        content: "Chad Flenderman. Solo un tipo negro tranquilo que conoce las calles, pero también fue a Oxford. Asi que tan cómodo en una motocicleta como en el Air Force One. Ah, y también es el egiptólogo más importante del mundo.",
+        character: {
+        id: "Toby",
+        firstname: "Toby",
+        lastname: "Flenderson",
+        },
+    },
+    {
+        id: 139,
+        content: "Las cámaras desechables son divertidas, pero parece un poco derrochador. Nunca llegas a ver tus fotos. Si es un evento importante que quieres recordar, te recomiendo usar una cámara real.",
+        character: {
+        id: "Erin",
+        firstname: "Erin",
+        lastname: "Hannon",
+        },
+    },
+    {
+        id: 140,
+        content: "Cállate sobre el sol. ¡CÁLLATE SOBRE EL SOL!",
+        character: {
+        id: "Gabe",
+        firstname: "Gabe",
+        lastname: "Lewis",
+        },
+    },
+    {
+        id: 141,
+        content: "En un mundo de pandillas, usamos algo llamado dedos esponjosos. Ahí es cuando alguien realmente se pone en tu cara, sabes que simplemente, comienzas a hacerles cosquillas. Y empieza a hacerte cosquillas. Y muy pronto tú riendo y abrazándote. Antes de que te des cuenta, te habrás olvidado de todo. Vayan todos juntos a la iglesia y coman un cono de helado.",
+        character: {
+        id: "Darryl",
+        firstname: "Darryl",
+        lastname: "Philbin",
+        },
+    }
+]
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 //Frases Random
-// fetch("https://rickandmortyapi.com/api/episode")
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data)
-// })
-// .catch( () => {
-//     console.log("No Salio")
-// })
+
+const divFrases=document.getElementById("random_frase_index")
+
+function shuffleArray(inputArray){
+    return inputArray.sort(()=> Math.random() - 0.5);
+}
+// function findFrase(array){
+//     const arrayJSON=JSON.stringify(array)
+//     const frase=arrayJSON.filter((frase)=> frase.id===0)
+//     return frase;
+// }
+
+const frasesRandom=()=>{
+    const arraymezclado=[shuffleArray(dunder)]
+
+
+    //     let divFrase=document.createElement("div")
+    //     divFrase.setAttribute("class","div__frase")
+    //     divFrases.append(divFrase)
+    //     let frase=document.createElement("p")
+    //     frase.setAttribute("class","div__frase__frase")
+    //     frase.innerText(`${dunder.content}`)
+    //     let autor=document.createElement("p")
+    //     autor.setAttribute("class","div__frase__autor")
+    //     autor.innerText(`${dunder.character.firstname} ${dunder.character.lastname}`)
+    //     divFrase.append(frase,autor)
+    // })
+}
+frasesRandom()
+function getRandom(min, max) {
+    return Math.round((Math.random() * (max - min) + min));
+}  
+console.log(getRandom(100,142))
