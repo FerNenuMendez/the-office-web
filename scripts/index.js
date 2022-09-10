5bc9ed699fc744e3aef885ed52382ba2d9471403
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2022-09-09 21:39:32 -0300

    Seccion Frases agregada

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,26 @@
 
 const divFrases=document.getElementById("random_frase_index")
 
-function shuffleArray(inputArray){
-    return inputArray.sort(()=> Math.random() - 0.5);
+function mezclarArray(inputArray){
+    inputArray.sort(()=> Math.random() - 0.5)
+    return inputArray[0];
 }
-// function findFrase(array){
-//     const arrayJSON=JSON.stringify(array)
-//     const frase=arrayJSON.filter((frase)=> frase.id===0)
-//     return frase;
-// }
 
-const frasesRandom=()=>{
-    const arraymezclado=[shuffleArray(dunder)]
+const frase=[mezclarArray(dunder)]
+console.log(frase)
 
-
-    //     let divFrase=document.createElement("div")
-    //     divFrase.setAttribute("class","div__frase")
-    //     divFrases.append(divFrase)
-    //     let frase=document.createElement("p")
-    //     frase.setAttribute("class","div__frase__frase")
-    //     frase.innerText(`${dunder.content}`)
-    //     let autor=document.createElement("p")
-    //     autor.setAttribute("class","div__frase__autor")
-    //     autor.innerText(`${dunder.character.firstname} ${dunder.character.lastname}`)
-    //     divFrase.append(frase,autor)
-    // })
+const fraseDom=()=>{
+    frase.forEach((frase)=>{
+        let h4=document.createElement("h4")
+        h4.innerText=(frase.content)
+        h4.setAttribute("class","frase__h4")
+        let p=document.createElement("p")
+        p.innerText=(frase.character.firstname)
+        p.setAttribute("class","frase__p")
+        let p2=document.createElement("p")
+        p2.innerText=(frase.character.lastname)
+        p2.setAttribute("class","frase__p")
+        divFrases.append(h4,p,p2)
+    })
 }
-frasesRandom()
-function getRandom(min, max) {
-    return Math.round((Math.random() * (max - min) + min));
-}  
-console.log(getRandom(100,142))
+fraseDom()
